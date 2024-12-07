a83b33c4617d99a95deac6ea462748643f9896d7
Janaina Cazuza <janainamartinscazuza@gmail.com>
2024-12-07 03:56:42 -0300

    Closing one more course

--- Python/Courses/Alura/poo/modelos/main_ap.py
+++ Python/Courses/Alura/poo/modelos/main_ap.py
@@ -1,13 +1,15 @@
 from restaurant import Restaurant
 
 restaurant_praca = Restaurant('Praça', 'Gourmet')
-restaurant_mexican = Restaurant('Mexican Food', 'Mexicana')
-restaurant_japanese = Restaurant('Japanese Food', 'Japonesa')
+restaurant_praca.add_evaluation('John', 8)
+restaurant_praca.add_evaluation('Alice', 9)
+restaurant_praca.add_evaluation('Bob', 7)
 
-restaurant_mexican.restaurant_status()
+restaurant_praca.restaurant_status()
 
 def main():
-    pass
+    """Main function."""
+    Restaurant.list_restaurants()
 
 if __name__ == '__main__':
     main()
